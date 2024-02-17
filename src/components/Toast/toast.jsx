7af8d09ffcfcb1efc909c0a47f888063d3c4e29c
youngeunkim <pkhj286@gmail.com>
2024-02-17 23:38:23 +0900

    애니메이션

--- src/components/Toast/toast.jsx
+++ src/components/Toast/toast.jsx
@@ -2,20 +2,23 @@ import React, { useState, useEffect } from "react";
 import * as S from "./style.js";
 import back from "../../assets/back.svg";
 
-const Toast = (text) => {
+const Toast = ({ text }) => {
   const [isVisible, setIsVisible] = useState(true);
 
   const handleBackClick = () => {
     setIsVisible(false);
   };
 
-  useEffect(() => {}, [isVisible]);
-
   return (
-    <S.Container isVisible={isVisible}>
-      <S.Text>{text}</S.Text>
-      <S.Back src={back} onClick={handleBackClick} />
-    </S.Container>
+    <>
+      {isVisible && (
+        <S.Container isVisible={isVisible}>
+          <S.Text>{text}</S.Text>
+          <S.Back src={back} onClick={handleBackClick} />
+        </S.Container>
+      )}
+      ;
+    </>
   );
 };
 
