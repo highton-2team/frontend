82236481bec2beb1ec8bb5f5322647ba0e2c8ad5
youngeunkim <pkhj286@gmail.com>
2024-02-17 21:45:05 +0900

    props

--- src/components/Toast/toast.jsx
+++ src/components/Toast/toast.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import * as S from "./style.js";
 import back from "../../assets/back.svg";
 
-const Toast = () => {
+const Toast = ({ title }) => {
   const [isVisible, setIsVisible] = useState(true);
 
   const handleBackClick = () => {
@@ -12,8 +12,8 @@ const Toast = () => {
   useEffect(() => {}, [isVisible]);
 
   return (
-    <S.Container isVisible={isVisible}>
-      <S.Text>6행 수량을 확인해 주세요</S.Text>
+    <S.Container>
+      <S.Text>{title}</S.Text>
       <S.Back src={back} onClick={handleBackClick} />
     </S.Container>
   );
